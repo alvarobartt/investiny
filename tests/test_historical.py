f7412ee54b72813e4bc62e19eacd900bb0f1a038
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-01 09:03:55 +0000

    🧪 Add `from_date` and `to_date` fixtures

--- tests/test_historical.py
+++ tests/test_historical.py
@@ -6,10 +6,10 @@ import pytest
 from investiny import historical_data  # noqa: F401
 
 
-@pytest.mark.usefixtures("investing_id")
-def test_historical(investing_id: int) -> None:
+@pytest.mark.usefixtures("investing_id", "from_date", "to_date")
+def test_historical(investing_id: int, from_date: str, to_date: str) -> None:
     res = historical_data(
-        investing_id=investing_id, from_date="01/01/2021", to_date="01/01/2022"
+        investing_id=investing_id, from_date=from_date, to_date=to_date
     )
     assert all(key in res.keys() for key in ["open", "high", "low", "close"])
     assert isinstance(res, dict)
