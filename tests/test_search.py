0012ab92a13d7f83759dca49a585fd5442507f1e
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-03 18:36:37 +0000

    🧪 Add `search_assets` unit tests

---
+++ tests/test_search.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Alvaro Bartolome, alvarobartt @ GitHub
+# See LICENSE for details.
+
+import pytest
+
+from investiny.search import search_assets
+
+
+@pytest.mark.usefixtures("query")
+def test_search(query: str) -> None:
+    res = search_assets(query=query, limit=10, type="Stock")
+    assert isinstance(res, list)
+    assert all(
+        key in res[0].keys()
+        for key in ["symbol", "full_name", "description", "type", "ticker", "exchange"]
+    )
+    assert isinstance(res[0], dict)
