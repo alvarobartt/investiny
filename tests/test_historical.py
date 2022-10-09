8bad65bdfcff6bd2a2f54cc3295f83d62d952b2d
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-09 18:45:51 +0000

    🧪 Add `historical_data` tests on wider date range

--- tests/test_historical.py
+++ tests/test_historical.py
@@ -24,3 +24,18 @@ def test_historical_without_dates(investing_id: int) -> None:
         key in res.keys() for key in ["date", "open", "high", "low", "close", "volume"]
     )
     assert isinstance(res, dict)
+
+
+@pytest.mark.usefixtures("investing_id", "from_date_wide_range", "to_date_wide_range")
+def test_historical_wide_range(
+    investing_id: int, from_date_wide_range: str, to_date_wide_range: str
+) -> None:
+    res = historical_data(
+        investing_id=investing_id,
+        from_date=from_date_wide_range,
+        to_date=to_date_wide_range,
+    )
+    assert all(
+        key in res.keys() for key in ["date", "open", "high", "low", "close", "volume"]
+    )
+    assert isinstance(res, dict)
