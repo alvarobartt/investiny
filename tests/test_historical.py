ba799e1de9d5ccf739418fab3fa71b439f178622
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-15 16:51:13 +0200

    🧪 Add `from_date`/`to_date` assertions

--- tests/test_historical.py
+++ tests/test_historical.py
@@ -3,7 +3,7 @@
 
 import pytest
 
-from investiny import historical_data  # noqa: F401
+from investiny import historical_data
 
 
 @pytest.mark.usefixtures("investing_id", "from_date", "to_date")
@@ -11,19 +11,21 @@ def test_historical(investing_id: int, from_date: str, to_date: str) -> None:
     res = historical_data(
         investing_id=investing_id, from_date=from_date, to_date=to_date
     )
+    assert isinstance(res, dict)
     assert all(
         key in res.keys() for key in ["date", "open", "high", "low", "close", "volume"]
     )
-    assert isinstance(res, dict)
+    assert res["date"][0] == from_date
+    assert res["date"][-1] == to_date
 
 
 @pytest.mark.usefixtures("investing_id")
 def test_historical_without_dates(investing_id: int) -> None:
     res = historical_data(investing_id=investing_id)
+    assert isinstance(res, dict)
     assert all(
         key in res.keys() for key in ["date", "open", "high", "low", "close", "volume"]
     )
-    assert isinstance(res, dict)
 
 
 @pytest.mark.usefixtures("investing_id", "from_date_wide_range", "to_date_wide_range")
@@ -35,7 +37,9 @@ def test_historical_wide_range(
         from_date=from_date_wide_range,
         to_date=to_date_wide_range,
     )
+    assert isinstance(res, dict)
     assert all(
         key in res.keys() for key in ["date", "open", "high", "low", "close", "volume"]
     )
-    assert isinstance(res, dict)
+    assert res["date"][0] == from_date_wide_range
+    assert res["date"][-1] == to_date_wide_range
