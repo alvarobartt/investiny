f9944ec0b7c276abc89cd6ed3c3793a7bf510860
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-16 12:27:26 +0200

    ✨ Add `investiny.info` import

--- src/investiny/__init__.py
+++ src/investiny/__init__.py
@@ -7,4 +7,5 @@ __author__ = "Alvaro Bartolome, alvarobartt @ GitHub"
 __version__ = "0.7.0"
 
 from investiny.historical import historical_data  # noqa: F401
+from investiny.info import info  # noqa: F401
 from investiny.search import search_assets  # noqa: F401
