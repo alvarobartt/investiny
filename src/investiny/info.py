d6cf29dbf978ffec0ec01737b18fe7a9751f7089
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-16 12:21:28 +0200

    💡 Add `info` docstrings

--- src/investiny/info.py
+++ src/investiny/info.py
@@ -7,16 +7,28 @@ from investiny.utils import request_to_investing
 
 
 def info(asset: Union[str, List[str]]) -> Dict[str, Any]:
+    """Get assets' public information available at Investing.com
+
+    Args:
+        asset:
+            name to retrieve its information from Investing.com. Note that this arg can be:
+            the name of a single asset, a comma-separated string with asset names, or a list
+            of asset names.
+
+    Returns:
+        A dictionary with the assets' public information available at Investing.com, where the
+        key is the asset name and the value is a dictionary with the asset's information.
+    """
     results = request_to_investing(
         endpoint="quotes",
         params={"symbols": asset if isinstance(asset, str) else ",".join(asset)},
     )
     if len(results["d"]) < 2:
         return {results["d"][0]["n"]: results["d"][0]["v"]}
-    info = {}
+    r = {}
     for result in results["d"]:
-        info[result["n"]] = result["v"]
-    return info
+        r[result["n"]] = result["v"]
+    return r
 
 
 def investing_info(investing_id: int) -> Dict[str, Any]:
