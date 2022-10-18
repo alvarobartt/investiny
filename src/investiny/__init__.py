f3ae49b4d39ebf8aa39d1970c614deb589d968ce
Alvaro Bartolome <alvarobartt@yahoo.com>
2022-10-18 16:08:07 +0200

    🔖 Set `investiny v0.7.2` release

--- src/investiny/__init__.py
+++ src/investiny/__init__.py
@@ -1,7 +1,7 @@
 """`investiny `: `investpy` but made tiny."""
 
 __author__ = "Alvaro Bartolome, alvarobartt @ GitHub"
-__version__ = "0.7.1"
+__version__ = "0.7.2"
 
 from investiny.historical import historical_data  # noqa: F401
 from investiny.info import info  # noqa: F401
